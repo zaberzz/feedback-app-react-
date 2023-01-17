069ac726b1454aab13840bb456bdb17a4b516728
Saba <saba.sadegh67@gmail.com>
2023-01-17 17:11:45 +0330

    add functionality to close button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import { useState } from "react";
+import React, { Component } from "react";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackData from "./data/FeedbackData";
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure you want to delete feedback?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
 
   return (
     <>
       <Header />
       <div className="container">
-        <FeedbackList feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
   );

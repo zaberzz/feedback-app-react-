818cf0fbd7305144a7d01b2d7edede26913e56e8
Saba <saba.sadegh67@gmail.com>
2023-01-17 16:23:42 +0330

    add conditional style

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,14 +1,18 @@
 import React from "react";
+import Card from "../shared/Card";
+import PropTypes from "prop-types";
 
 function FeedbackItem({ item }) {
   return (
-    <div>
-      <div className="card">
-        <div className="num-display">{item.rating}</div>
-        <div className="text-display">{item.text}</div>
-      </div>
-    </div>
+    <Card reverse={true}>
+      <div className="num-display">{item.rating}</div>
+      <div className="text-display">{item.text}</div>
+    </Card>
   );
 }
 
 export default FeedbackItem;
+
+FeedbackItem.propTypes = {
+  item: PropTypes.object.isRequired,
+};

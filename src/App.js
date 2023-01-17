24521783f08e05cbfb072116c0a60109277d4845
Saba <saba.sadegh67@gmail.com>
2023-01-17 17:26:41 +0330

    calculating average rating

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import React, { Component } from "react";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
+import FeedbackStats from "./components/FeedbackStats";
 import FeedbackData from "./data/FeedbackData";
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -15,6 +16,7 @@ function App() {
     <>
       <Header />
       <div className="container">
+        <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>

0c549a62a71da6310ebac19b78afed0d8cebf64d
Saba <saba.sadegh67@gmail.com>
2023-01-18 10:41:42 +0330

    validating user comments

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,25 +1,44 @@
 import React, { useState } from "react";
 import Card from "../shared/Card";
 import Button from "../shared/Button";
+import RatingSelect from "./RatingSelect";
 
 function FeedbackForm() {
   const [text, setText] = useState("");
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  
+  const [message, setMessage] = useState();
   const handleTextChange = (e) => {
-    setText(e.target.value);
+    if (text === "") {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== "" && text.trim().length <= 10) setText(e.target.value);
+    {
+      setBtnDisabled(true);
+      setMessage("message must be at least 10 character");
+    } else {
+        setMessage(null)
+        setBtnDisabled(false)
+    }
+    setMessage(e.target.value)
   };
   return (
     <Card>
       <form>
         <h2>How would you rate the service with us?</h2>
+        <RatingSelect />
         <div className="input-group">
           <input
             onChange={handleTextChange}
             placeholder="Write your review"
             value={text}
           ></input>
-          <Button type="submit">Send</Button>
+          <Button type="submit" isDisabled={btnDisabled}>
+            Send
+          </Button>
         </div>
       </form>
+      {message && <div className="message">{message}</div>}
     </Card>
   );
 }

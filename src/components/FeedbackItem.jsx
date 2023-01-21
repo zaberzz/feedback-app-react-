e461feb61659eb2e631776d45ec7cc74bf9d710c
Saba <saba.sadegh67@gmail.com>
2023-01-21 22:10:10 +0330

    add edit btn

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,17 +1,20 @@
 import React from "react";
-import { FaTimes } from "react-icons/fa";
+import { FaTimes, FaEdit } from "react-icons/fa";
 import { useContext } from "react";
 import Card from "../shared/Card";
 import PropTypes from "prop-types";
 import FeedbackContext from "../context/FeedbackContext";
 
 function FeedbackItem({ item }) {
-  const { deleteFeedback } = useContext(FeedbackContext);
+  const { deleteFeedback, editFeedback } = useContext(FeedbackContext);
   return (
     <Card reverse={true}>
       <div className="num-display">{item.rating}</div>
       <button onClick={() => deleteFeedback(item.id)} className="close">
         <FaTimes color="purple" />
+      </button>
+      <button className="edit" onClick={() => editFeedback(item)}>
+        <FaEdit color="purple" />
       </button>
       <div className="text-display">{item.text}</div>
     </Card>
